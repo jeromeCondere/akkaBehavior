f8128318e04a64dc83062fb74286fa04bd392515
jegr <jerome.ceg>
2017-01-25 18:02:05 +0100

    must refactor test code

--- src/main/scala/behavior/ParralelBehavior.scala
+++ src/main/scala/behavior/ParralelBehavior.scala
@@ -1,7 +1,7 @@
 package behavior
 import akka.actor.ActorRef
 class ParralelBehavior(behaviorList:List[AbstractBehavior]) (toRun:() =>Unit)(implicit supervisor:ActorRef) extends AbstractBehavior(toRun){
-  override def run ={
-
+  override def run = {
+  
   }
 }
