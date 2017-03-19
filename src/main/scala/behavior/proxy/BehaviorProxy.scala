342bfbe32d293a0631543fe728ec57d495a4cdcf
Condere Jerome <jerome.condere@gmail.com>
2017-03-19 08:44:43 +0100

    removing useless function (BehaviorProxy)

--- src/main/scala/behavior/proxy/BehaviorProxy.scala
+++ src/main/scala/behavior/proxy/BehaviorProxy.scala
@@ -4,9 +4,7 @@ import behavior.AbstractBehavior
  *  a class that allows the user to store a future instance of a behavior
  */
 final class BehaviorProxy  [A <: AbstractBehavior] (val behavior:() => A) {
-  def apply(f:() => A) = {
-    new BehaviorProxy(f)
-  }
+
 }
 
 object BehaviorProxy {
