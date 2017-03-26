9dcde6f54fd3c03999cab5115d0a18ec90c99d79
Condere Jerome <jerome.condere@gmail.com>
2017-03-26 10:09:37 +0200

    fixing bug on parrallelBehavior \n must do tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,8 @@
 import behavior._
 import akka.actor._
-import scala.concurrent.duration._
 
-class myActor extends Actor
-{
-  def receive = {
-    case _ => println("a")
-  }
-}
 
 object Main extends App {
  val system = ActorSystem("system")
- 
 
- 
 }
--- src/main/scala/behavior/ParralelBehavior.scala
+++ src/main/scala/behavior/ParralelBehavior.scala
@@ -12,19 +12,18 @@ import scala.reflect._
  * @param toRun the callback used to run the behavior
  * @param supervisor reference to the actor that use the behavior
  */
-class ParralelBehavior[A <: AbstractBehavior](behaviorProxyList:List[BehaviorProxy[A]]) (toRun:() =>Unit)(implicit supervisor:ActorRef) extends AbstractBehavior(toRun){
+class ParralelBehavior[A <: AbstractBehavior : ClassTag](behaviorProxyList:List[BehaviorProxy[A]]) (implicit supervisor:ActorRef) extends AbstractBehavior(() => {}){
  
   //to do find a way to override init (if not it won't be used in setup)
   /** setup all Behaviors */
-   final protected def init(implicit c: ClassTag[A]) = {
+  override final protected def init = {
    behaviorProxyList.zipWithIndex.foreach{
       case(behaviorProxy,index) => val actor = context.actorOf(Props(behaviorProxy.behavior()), self.path.name +"parallel"+index )
                               context.watch(actor)
                               actor ! Setup 
     }
-   println("issou")
   }
-   
+  
 
   /** run all behavior in the list of behaviors */
   override def run = {
